a9412fdbe1681a47471f2a63504b686d7fd3cf7e
Istewak Hassan Tewak <121757458+istewakhassantewak@users.noreply.github.com>
2026-05-14 18:26:35 +0600

    latest post fetching

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ async function fetchAllPost() {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
     const data = await res.json();
     const persons = data.posts
-    console.log(persons)
     persons.forEach(person => {
         const newDiv = document.createElement('div')
         newDiv.classList = `bg-[#f3f3f5] p-6 rounded-[32px] w-full  border border-gray-100 shadow-sm`;
@@ -61,15 +60,18 @@ async function fetchAllPost() {
                                     </div>
 
                                     <!-- Green Bookmark/Action Button -->
-                                    <div
+                                    <button
                                         class="rounded-full text-white cursor-pointer hover:bg-[#059669] transition-colors mt-5 md:mt-0">
-                                        <img src="images/click.png" class="w-11" alt="">
-                                        </svg>
-                                    </div>
+                                        <img src="images/click.png" onclick="loadData('${person.description}','${person.view_count}')"
+                                            class="w-11" alt="">
+
+                                    </button>
                                 </div>
                             </div>
                         </div>`
+
         allPostCard.append(newDiv)
+
         const active = document.getElementById(`a${person.id}`);
         if (person.isActive) {
             active.classList.add('bg-[#10B981]')
@@ -80,3 +82,55 @@ async function fetchAllPost() {
 
 }
 fetchAllPost()
+
+function loadData(desc, views) {
+    const bookMark = document.getElementById('bookmark')
+    const newDiv = document.createElement('div')
+    newDiv.classList = `bg-white p-6 rounded-3xl flex justify-between items-center shadow-sm`;
+    newDiv.innerHTML = `<h3 class="text-[#12132d] font-bold text-lg leading-snug max-w-[70%]">
+                                    ${desc}
+                                </h3>
+                                <div class="flex items-center gap-2 text-gray-500">
+                                    <img src="images/view.png" alt="">
+                                    <span class="text-lg">${views}</span>
+                                </div>`
+    bookMark.append(newDiv);
+
+
+}
+
+async function fetchLatestPost() {
+    const latestPost = document.getElementById('latest-post')
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const data = await res.json();
+    console.log(data)
+    data.forEach(data => {
+        const newDiv = document.createElement('div');
+        newDiv.classList = `p-6 border border-gray-100 rounded-[32px] shadow-sm bg-white space-y-4`
+        newDiv.innerHTML = `<div class="aspect-video bg-[#F3F3F3] rounded-2xl w-full">
+    <img src="${data.cover_image}"
+                                class="w-full h-full rounded-2xl" alt="">
+                                </div>
+                        <div class="flex items-center gap-2 text-gray-500 text-sm">
+                            <img src="images/calender.png" alt="">
+                            <span>${data.author?.posted_date || "No Publish Date"}</span >
+                        </div >
+                        <h3 class="text-xl font-extrabold text-[#12132D] leading-tight">
+                            ${data.title}
+                        </h3>
+                        <p class="text-gray-500 text-sm leading-relaxed">
+                            ${data.description}
+                        </p>
+                        <div class="flex items-center gap-3 pt-2">
+                            <div class="w-10 h-10 rounded-full bg-gray-200 overflow-hidden border border-gray-100">
+                                <img src="${data.profile_image}" alt="">
+                            </div>
+                            <div>
+                                <p class="font-bold text-[#12132D] text-sm">${data.author.name}</p>
+                                <p class="text-gray-400 text-xs">${data.author?.designation || "Unknown"}</p>
+                            </div>
+                        </div>`
+        latestPost.append(newDiv)
+    });
+}
+fetchLatestPost()
---
+++ js/utlity.js
@@ -0,0 +1,10 @@
+function addClass1(item, class1) {
+    const element = document.getElementById(item);
+    element.classList.add(class1);
+
+}
+function removeClass1(item, class1) {
+    const element = document.getElementById(item);
+    element.classList.remove(class1);
+
+}
